8edc7191fbee7eb5fc08133fd4beb6f5e9912011
Yue Peng <ypeng7@outlook.com>
2019-06-12 22:26:42 +0800

    npm install command

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 git clone https://github.com/k-takata/minpac.git ./pack/minpac/opt/minpac
 
 ln -s ~/nvim ~/.config/nvim
+
+# npm language server
+npm i -g bash-language-server
+npm install -g dockerfile-language-server-nodejs
+npm install -g typescript typescript-language-server
+npm install --global vscode-html-languageserver-bin
+npm install -g json-language-server
+npm install -g yaml-language-server
