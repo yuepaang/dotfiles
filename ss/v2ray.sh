0d8b88ae073617f4e51945c75b37813902fa2c3b
Yue Peng <yuepaang@gmail.com>
2021-07-27 00:23:40 +0800

    new layout

--- ss/v2ray.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-setcap cap_net_bind_service+ep /usr/local/bin/v2ray-plugin
-
-setcap cap_net_bind_service+ep /usr/bin/ss-server
--- zsh/.config/zsh/.zshrc
+++
@@ -1,4 +0,0 @@
-module_path+=( "$ZDOTDIR/zinit/bin/zmodules/Src" )
-zmodload zdharma/zplugin
-
-source $ZDOTDIR/zshrc.zsh
