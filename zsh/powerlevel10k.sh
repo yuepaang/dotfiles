85b7b7c0a132fa888de8bb6239ee1b83aad58757
Yue Peng <yuepaang@gmail.com>
2022-03-03 18:47:52 +0800

    new zsh

--- zsh/powerlevel10k.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-echo 'ZSH_THEME=powerlevel10k/powerlevel10k' >>! ~/.zshrc
-
-# Configuraion
-p10k configure
