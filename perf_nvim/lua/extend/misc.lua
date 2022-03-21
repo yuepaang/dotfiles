df84fcd83d2e53152cb6c42fba0277c3bbc5acc9
Yue Peng <yuepaang@gmail.com>
2022-03-21 14:32:55 +0800

    update latest (commit ae6aaa36391cf965a299afd99bca83d450a083ab)

--- perf_nvim/lua/extend/misc.lua
+++ perf_nvim/lua/extend/misc.lua
@@ -1,30 +1,33 @@
 local misc = {}
 
 misc.safe_exit = function()
-	if (not packer_plugins['vim-floaterm'].loaded) or (not packer_plugins['nvim-tree.lua'].loaded) then
-		vim.cmd [[PackerLoad vim-floaterm]]
-		vim.cmd [[PackerLoad nvim-tree.lua]]
+	if not packer_plugins["vim-floaterm"].loaded or not packer_plugins["nvim-tree.lua"].loaded then
+		vim.cmd([[PackerLoad vim-floaterm]])
+		vim.cmd([[PackerLoad nvim-tree.lua]])
 	end
 
 	-- close floaterm
 	local floatermBufnr = vim.call("floaterm#buflist#gather")
 	if #floatermBufnr ~= 0 then
-		vim.cmd[[FloatermKill]]
+		vim.cmd([[FloatermKill]])
 	end
 
 	-- close nvim-tree
-	local view = require'nvim-tree.view'
+	local view = require("nvim-tree.view")
 	if view.is_visible() then
-		require('extend.tree').toggle()
+		require("extend.tree").toggle()
 	end
 
 	-- quit
-	vim.cmd[[confirm xa]]
+	vim.cmd([[confirm qa]])
 end
 
 misc.safe_save = function()
-	vim.cmd[[write]]
-	vim.cmd[[SaveSession]]
+	if not packer_plugins["auto-session"].loaded then
+		vim.cmd([[PackerLoad auto-session]])
+	end
+	vim.cmd([[write]])
+	require("auto-session").SaveSession()
 end
 
 return misc
--- perf_nvim/lua/modules/tools/config.lua
+++ perf_nvim/lua/modules/tools/config.lua
@@ -1,24 +1,27 @@
 local config = {}
 
 function config.telescope()
-  if (not packer_plugins['telescope-fzy-native.nvim'].loaded) or (not packer_plugins['telescope-file-browser.nvim'].loaded) then
-	vim.cmd [[PackerLoad telescope-fzy-native.nvim]]
-	vim.cmd [[PackerLoad telescope-file-browser.nvim]]
-  end
+	if
+		not packer_plugins["telescope-fzy-native.nvim"].loaded
+		or not packer_plugins["telescope-file-browser.nvim"].loaded
+	then
+		vim.cmd([[PackerLoad telescope-fzy-native.nvim]])
+		vim.cmd([[PackerLoad telescope-file-browser.nvim]])
+	end
 
-	local actions = require "telescope.actions"
-	local actions_layout = require "telescope.actions.layout"
+	local actions = require("telescope.actions")
+	local actions_layout = require("telescope.actions.layout")
 
-	require('telescope').setup {
+	require("telescope").setup({
 		defaults = {
 			initial_mode = "normal",
-			prompt_prefix = '🔭 ',
+			prompt_prefix = "🔭 ",
 			selection_caret = " ",
-			sorting_strategy = 'ascending',
+			sorting_strategy = "ascending",
 			scroll_strategy = "limit",
-			set_env = { ['COLORTERM'] = 'truecolor'},
+			set_env = { ["COLORTERM"] = "truecolor" },
 			path_display = {
-				shorten = { len = 2, exclude = {-2, -1}}
+				shorten = { len = 2, exclude = { -2, -1 } },
 			},
 			results_title = true,
 			color_devicons = true,
@@ -30,7 +33,7 @@ function config.telescope()
 				"--line-number",
 				"--column",
 				"--smart-case",
-				"--trim" -- add this value
+				"--trim", -- add this value
 			},
 			layout_strategy = "flex",
 			layout_config = {
@@ -39,15 +42,15 @@ function config.telescope()
 					height = 0.9,
 					preview_cutoff = 120,
 					preview_width = 0.45,
-					prompt_position = "top"
+					prompt_position = "top",
 				},
 				vertical = {
 					width = 0.9,
 					height = 0.9,
-				}
+				},
 			},
 			preview = {
-				hide_on_startup = false
+				hide_on_startup = false,
 			},
 			default_mappings = {
 				i = {
@@ -113,87 +116,87 @@ function config.telescope()
 				},
 			},
 		},
-	}
-	require('telescope').load_extension('fzy_native')
-	require('telescope').load_extension('file_browser')
-	require('telescope').load_extension('todo-comments')
-	require('telescope').load_extension('projects')
+	})
+	require("telescope").load_extension("fzy_native")
+	require("telescope").load_extension("file_browser")
+	require("telescope").load_extension("todo-comments")
+	require("telescope").load_extension("projects")
 end
 
 function config.nvim_tree()
 	vim.g.nvim_tree_respect_buf_cwd = 1
-	require'nvim-tree'.setup {
-	  -- disables netrw completely
-	  disable_netrw       = true,
-	  -- hijack netrw window on startup
-	  hijack_netrw        = true,
-	  -- open the tree when running this setup function
-	  open_on_setup       = false,
-	  -- will not open on setup if the filetype is in this list
-	  ignore_ft_on_setup  = {},
-	  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-	  auto_close          = false,
-	  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-	  auto_reload_on_write = true,
-	  open_on_tab         = false,
-	  -- hijack the cursor in the tree to put it at the start of the filename
-	  hijack_cursor       = true,
-	  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-	  update_cwd          = true,
-	  -- show lsp diagnostics in the signcolumn
-	  update_to_buf_dir   = {
-		enable = true,
-		auto_open = true,
-	  },
-	  diagnostics = {
-		enable = true,
-		icons = {
-		  hint = "",
-		  info = "",
-		  warning = "",
-		  error = "",
-		}
-	  },
-	  git = {
-		enable = true,
-		ignore = false,
-		timeout = 500,
-	  },
-	  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-	  update_focused_file = {
-		-- enables the feature
-		enable      = true,
-		-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-		-- only relevant when `update_focused_file.enable` is true
-		update_cwd  = true,
-		-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-		-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-		ignore_list = {}
-	  },
-	  -- configuration options for the system open command (`s` in the tree by default)
-	  system_open = {
-		-- the command to run this, leaving nil should work in most cases
-		cmd  = nil,
-		-- the command arguments as a list
-		args = {}
-	  },
-	  view = {
-		width = 31,
-		side = 'left',
-		-- if true the tree will resize itself after opening a file
-		auto_resize = false,
-		mappings = {
-		  -- custom only false will merge the list with the default mappings
-		  -- if true, it will only use your list to set the mappings
-		  custom_only = false,
-		  -- list of mappings to set on the tree manually
-		  list = {}
+	require("nvim-tree").setup({
+		-- disables netrw completely
+		disable_netrw = true,
+		-- hijack netrw window on startup
+		hijack_netrw = true,
+		-- open the tree when running this setup function
+		open_on_setup = false,
+		-- will not open on setup if the filetype is in this list
+		ignore_ft_on_setup = {},
+		-- closes neovim automatically when the tree is the last **WINDOW** in the view
+		auto_close = false,
+		-- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+		auto_reload_on_write = true,
+		open_on_tab = false,
+		-- hijack the cursor in the tree to put it at the start of the filename
+		hijack_cursor = true,
+		-- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+		update_cwd = true,
+		-- show lsp diagnostics in the signcolumn
+		update_to_buf_dir = {
+			enable = true,
+			auto_open = true,
 		},
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes"
-	  }
-	}
+		diagnostics = {
+			enable = true,
+			icons = {
+				hint = "",
+				info = "",
+				warning = "",
+				error = "",
+			},
+		},
+		git = {
+			enable = true,
+			ignore = false,
+			timeout = 500,
+		},
+		-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+		update_focused_file = {
+			-- enables the feature
+			enable = true,
+			-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+			-- only relevant when `update_focused_file.enable` is true
+			update_cwd = true,
+			-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+			-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+			ignore_list = {},
+		},
+		-- configuration options for the system open command (`s` in the tree by default)
+		system_open = {
+			-- the command to run this, leaving nil should work in most cases
+			cmd = nil,
+			-- the command arguments as a list
+			args = {},
+		},
+		view = {
+			width = 31,
+			side = "left",
+			-- if true the tree will resize itself after opening a file
+			auto_resize = false,
+			mappings = {
+				-- custom only false will merge the list with the default mappings
+				-- if true, it will only use your list to set the mappings
+				custom_only = false,
+				-- list of mappings to set on the tree manually
+				list = {},
+			},
+			number = false,
+			relativenumber = false,
+			signcolumn = "yes",
+		},
+	})
 end
 
 function config.symbols_outline()
@@ -202,16 +205,16 @@ function config.symbols_outline()
 		highlight_hovered_item = true,
 		show_guides = true,
 		auto_preview = false,
-		position = 'right',
+		position = "right",
 		relative_width = true,
 		width = 30,
 		auto_close = true,
 		show_numbers = false,
 		show_relative_numbers = false,
 		show_symbol_details = true,
-		preview_bg_highlight = 'LspSagaAutoPreview',
+		preview_bg_highlight = "LspSagaAutoPreview",
 		keymaps = { -- These keymaps can be a string or a table for multiple keys
-			close = {"<Esc>", "q"},
+			close = { "<Esc>", "q" },
 			goto_location = "<CR>",
 			focus_location = "o",
 			hover_symbol = "gh",
@@ -222,44 +225,44 @@ function config.symbols_outline()
 		lsp_blacklist = {},
 		symbol_blacklist = {},
 		symbols = {
-			File = {icon = icons.cmp.File, hl = "TSURI"},
-			Module = {icon = icons.cmp.Module, hl = "TSNamespace"},
-			Namespace = {icon = icons.cmp.Namespace, hl = "TSNamespace"},
-			Package = {icon = icons.cmp.Package, hl = "TSNamespace"},
-			Class = {icon = icons.cmp.Class, hl = "TSType"},
-			Method = {icon = icons.cmp.Method, hl = "TSMethod"},
-			Property = {icon = icons.cmp.Property, hl = "TSMethod"},
-			Field = {icon = icons.cmp.Field, hl = "TSField"},
-			Constructor = {icon = icons.cmp.Constructor, hl = "TSConstructor"},
-			Enum = {icon = icons.cmp.Enum, hl = "TSType"},
-			Interface = {icon = icons.cmp.Interface, hl = "TSType"},
-			Function = {icon = icons.cmp.Function, hl = "TSFunction"},
-			Variable = {icon = icons.cmp.Variable, hl = "TSConstant"},
-			Constant = {icon = icons.cmp.Constant, hl = "TSConstant"},
-			String = {icon = icons.cmp.String, hl = "TSString"},
-			Number = {icon = icons.cmp.Number, hl = "TSNumber"},
-			Boolean = {icon = icons.cmp.Boolean, hl = "TSBoolean"},
-			Array = {icon = icons.cmp.Array, hl = "TSConstant"},
-			Object = {icon = icons.cmp.Object, hl = "TSType"},
-			Key = {icon = icons.cmp.Keyword, hl = "TSType"},
-			Null = {icon = icons.cmp.Null, hl = "TSType"},
-			EnumMember = {icon = icons.cmp.EnumMember, hl = "TSField"},
-			Struct = {icon = icons.cmp.Struct, hl = "TSType"},
-			Event = {icon = icons.cmp.Event, hl = "TSType"},
-			Operator = {icon = icons.cmp.Operator, hl = "TSOperator"},
-			TypeParameter = {icon = icons.cmp.TypeParameter, hl = "TSParameter"}
-		}
+			File = { icon = icons.cmp.File, hl = "TSURI" },
+			Module = { icon = icons.cmp.Module, hl = "TSNamespace" },
+			Namespace = { icon = icons.cmp.Namespace, hl = "TSNamespace" },
+			Package = { icon = icons.cmp.Package, hl = "TSNamespace" },
+			Class = { icon = icons.cmp.Class, hl = "TSType" },
+			Method = { icon = icons.cmp.Method, hl = "TSMethod" },
+			Property = { icon = icons.cmp.Property, hl = "TSMethod" },
+			Field = { icon = icons.cmp.Field, hl = "TSField" },
+			Constructor = { icon = icons.cmp.Constructor, hl = "TSConstructor" },
+			Enum = { icon = icons.cmp.Enum, hl = "TSType" },
+			Interface = { icon = icons.cmp.Interface, hl = "TSType" },
+			Function = { icon = icons.cmp.Function, hl = "TSFunction" },
+			Variable = { icon = icons.cmp.Variable, hl = "TSConstant" },
+			Constant = { icon = icons.cmp.Constant, hl = "TSConstant" },
+			String = { icon = icons.cmp.String, hl = "TSString" },
+			Number = { icon = icons.cmp.Number, hl = "TSNumber" },
+			Boolean = { icon = icons.cmp.Boolean, hl = "TSBoolean" },
+			Array = { icon = icons.cmp.Array, hl = "TSConstant" },
+			Object = { icon = icons.cmp.Object, hl = "TSType" },
+			Key = { icon = icons.cmp.Keyword, hl = "TSType" },
+			Null = { icon = icons.cmp.Null, hl = "TSType" },
+			EnumMember = { icon = icons.cmp.EnumMember, hl = "TSField" },
+			Struct = { icon = icons.cmp.Struct, hl = "TSType" },
+			Event = { icon = icons.cmp.Event, hl = "TSType" },
+			Operator = { icon = icons.cmp.Operator, hl = "TSOperator" },
+			TypeParameter = { icon = icons.cmp.TypeParameter, hl = "TSParameter" },
+		},
 	}
 end
 
 function config.mkdp()
-  vim.g.mkdp_auto_start = 0
-  vim.g.mkdp_open_to_the_world  = 1
-  vim.g.mkdp_open_ip = '0.0.0.0'
-  vim.g.mkdp_port = 9096
-  vim.g.mkdp_echo_preview_url = 1
-  vim.g.mkdp_command_for_global = 1
-  vim.g.mkdp_auto_close = 0
+	vim.g.mkdp_auto_start = 0
+	vim.g.mkdp_open_to_the_world = 1
+	vim.g.mkdp_open_ip = "0.0.0.0"
+	vim.g.mkdp_port = 9096
+	vim.g.mkdp_echo_preview_url = 1
+	vim.g.mkdp_command_for_global = 1
+	vim.g.mkdp_auto_close = 0
 end
 
 function config.floaterm()
@@ -270,11 +273,11 @@ function config.floaterm()
 end
 
 function config.translator()
-	vim.g.translator_window_borderchars = {"─","│","─","│","╭","╮","╯","╰"}
+	vim.g.translator_window_borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
 end
 
 function config.project()
-	require("project_nvim").setup {
+	require("project_nvim").setup({
 		-- Manual mode doesn't automatically change your root directory, so you have
 		-- the option to manually do so using `:ProjectRoot` command.
 		manual_mode = false,
@@ -318,23 +321,23 @@ function config.project()
 		-- Note: All will change the directory regardless
 		telescope_on_project_selected = function(path, open)
 			require("auto-session").RestoreSession()
-		end
-	}
+		end,
+	})
 end
 
 function config.autosession()
-	vim.o.sessionoptions="buffers,curdir,folds,help,tabpages,winsize,winpos"
-	require('auto-session').setup({
-		log_level = 'info',
+	vim.o.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,winpos"
+	require("auto-session").setup({
+		log_level = "info",
 		auto_session_enable_last_session = false,
-		auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+		auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
 		auto_session_enabled = false,
 		auto_save_enabled = false,
 		auto_restore_enabled = false,
-		auto_session_suppress_dirs = {'~/'},
+		auto_session_suppress_dirs = {},
 		-- the configs below are lua only
 		bypass_session_save_file_types = nil,
-		post_restore_cmds = { require('extend.tree').toggle }
+		post_restore_cmds = { require("extend.tree").toggle },
 	})
 end
 
@@ -342,27 +345,27 @@ function config.which_key()
 	local wk = require("which-key")
 	wk.setup({
 		key_labels = {
-		["<space>"] = "SPC",
-		["<leader>"] = "SPC",
-		["<cr>"] = "ENT",
-		["<tab>"] = "TAB",
-		["<a>"] = "ALT",
-		["<s>"] = "SHI",
-		["<c>"] = "CTR",
-	  },
-	  operators = {},
-	  window = {
-		border = "single", -- none, single, double, shadow
-		position = "bottom", -- bottom, top
-		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-		winblend = 0
-	  },
-	  ignore_missing = false,
+			["<space>"] = "SPC",
+			["<leader>"] = "SPC",
+			["<cr>"] = "ENT",
+			["<tab>"] = "TAB",
+			["<a>"] = "ALT",
+			["<s>"] = "SHI",
+			["<c>"] = "CTR",
+		},
+		operators = {},
+		window = {
+			border = "single", -- none, single, double, shadow
+			position = "bottom", -- bottom, top
+			margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+			padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+			winblend = 0,
+		},
+		ignore_missing = false,
 	})
 
 	-- bind common keymap
-	local bind = require('keymap.bind')
+	local bind = require("keymap.bind")
 	local def_map = require("keymap.def_map")
 	local plug_map = require("keymap.plug_map")
 
