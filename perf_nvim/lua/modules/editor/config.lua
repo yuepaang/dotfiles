cc792f7b57272e95b5dd1acc56ce83070deddbcd
Yue Peng <yuepaang@gmail.com>
2022-03-29 00:42:10 +0800

    update 0b4a62d

--- perf_nvim/lua/modules/editor/config.lua
+++ perf_nvim/lua/modules/editor/config.lua
@@ -1,96 +1,100 @@
 local config = {}
 
 function config.todo()
-	require("todo-comments").setup{
-	 signs = true, -- show icons in the signs column
-	  sign_priority = 8, -- sign priority
-	  -- keywords recognized as todo comments
-	  keywords = {
-		FIX = {
-			icon = " ", -- icon used for the sign, and in search results
-			color = "error", -- can be a hex color, or a named color (see below)
-			alt = {
-				"FIXME", "BUG", "FIXIT", "ISSUE", "ERROR",
-			}, -- a set of other keywords that all map to this FIX keywords
-		  -- signs = false, -- configure signs for some keywords individually
+	require("todo-comments").setup({
+		signs = true, -- show icons in the signs column
+		sign_priority = 8, -- sign priority
+		-- keywords recognized as todo comments
+		keywords = {
+			FIX = {
+				icon = " ", -- icon used for the sign, and in search results
+				color = "error", -- can be a hex color, or a named color (see below)
+				alt = {
+					"FIXME",
+					"BUG",
+					"FIXIT",
+					"ISSUE",
+					"ERROR",
+				}, -- a set of other keywords that all map to this FIX keywords
+				-- signs = false, -- configure signs for some keywords individually
+			},
+			TODO = { icon = " ", color = "info" },
+			HACK = { icon = " ", color = "warning" },
+			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			PERF = { icon = " ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
 		},
-		TODO = { icon = " ", color = "info" },
-		HACK = { icon = " ", color = "warning" },
-		WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-		PERF = { icon = " ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-		NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-	  },
-	  merge_keywords = true, -- when true, custom keywords will be merged with the defaults
-	  -- highlighting of the line containing the todo comment
-	  -- * before: highlights before the keyword (typically comment characters)
-	  -- * keyword: highlights of the keyword
-	  -- * after: highlights after the keyword (todo text)
-	  highlight = {
-		before = "", -- "fg" or "bg" or empty
-		keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-		after = "fg", -- "fg" or "bg" or empty
-		pattern = [[.*<(KEYWORDS)\s*:+\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
-		comments_only = true, -- uses treesitter to match keywords in comments only
-		max_line_len = 400, -- ignore lines longer than this
-		exclude = {}, -- list of file types to exclude highlighting
-	  },
-	  -- list of named colors where we try to extract the guifg from the
-	  -- list of hilight groups or use the hex color if hl not found as a fallback
-	  colors = {
-		error = {"#DC2626" },
-		warning = {"#FBBF24" },
-		info = {"#2563EB" },
-		hint = {"#10B981" },
-		default = {"#7C3AED" },
-	  },
-	  search = {
-		command = "rg",
-		args = {
-		  "--color=never",
-		  "--no-heading",
-		  "--with-filename",
-		  "--line-number",
-		  "--column",
+		merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+		-- highlighting of the line containing the todo comment
+		-- * before: highlights before the keyword (typically comment characters)
+		-- * keyword: highlights of the keyword
+		-- * after: highlights after the keyword (todo text)
+		highlight = {
+			before = "", -- "fg" or "bg" or empty
+			keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+			after = "fg", -- "fg" or "bg" or empty
+			pattern = [[.*<(KEYWORDS)\s+\(.*\):+\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
+			comments_only = true, -- uses treesitter to match keywords in comments only
+			max_line_len = 400, -- ignore lines longer than this
+			exclude = {}, -- list of file types to exclude highlighting
 		},
-		-- regex that will be used to match keywords.
-		-- don't replace the (KEYWORDS) placeholder
-		pattern = [[\b(KEYWORDS)\s*:]], -- ripgrep regex
-		-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-	  },
-	}
+		-- list of named colors where we try to extract the guifg from the
+		-- list of hilight groups or use the hex color if hl not found as a fallback
+		colors = {
+			error = { "#DC2626" },
+			warning = { "#FBBF24" },
+			info = { "#2563EB" },
+			hint = { "#10B981" },
+			default = { "#7C3AED" },
+		},
+		search = {
+			command = "rg",
+			args = {
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+			},
+			-- regex that will be used to match keywords.
+			-- don't replace the (KEYWORDS) placeholder
+			pattern = [[\b(KEYWORDS)\s*:]], -- ripgrep regex
+			-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+		},
+	})
 end
 
 function config.comment()
-	require('Comment').setup{
+	require("Comment").setup({
 		padding = true,
 		sticky = true,
 		ignore = nil,
-	}
+	})
 end
 
 function config.autopairs()
-	require('utils.defer').load_immediately('nvim-cmp')
+	require("utils.defer").load_immediately("nvim-cmp")
 
 	local cmp = require("cmp")
 
-	require('nvim-autopairs').setup{}
+	require("nvim-autopairs").setup({})
 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 function config.neoscroll()
-	require('neoscroll').setup({
+	require("neoscroll").setup({
 		-- All these keys will be mapped to their corresponding default scrolling animation
-		mappings = {'<C-f>', '<C-b>'},
-		hide_cursor = false,          -- Hide cursor while scrolling
-		stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+		mappings = { "<C-f>", "<C-b>" },
+		hide_cursor = false, -- Hide cursor while scrolling
+		stop_eof = true, -- Stop at <EOF> when scrolling downwards
 		use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-		respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+		respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
 		cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-		easing_function = nil,        -- Default easing function
-		pre_hook = nil,              -- Function to run before the scrolling animation starts
-		post_hook = nil,              -- Function to run after the scrolling animation ends
-		performance_mode = false
+		easing_function = nil, -- Default easing function
+		pre_hook = nil, -- Function to run before the scrolling animation starts
+		post_hook = nil, -- Function to run after the scrolling animation ends
+		performance_mode = false,
 	})
 end
 
@@ -129,11 +133,11 @@ function config.barbar()
 		icon_custom_colors = false,
 
 		-- Configure icons on the bufferline.
-		icon_separator_active = '│',
-		icon_separator_inactive = '│',
-		icon_close_tab = '',
-		icon_close_tab_modified = '●',
-		icon_pinned = '車',
+		icon_separator_active = "│",
+		icon_separator_inactive = "│",
+		icon_close_tab = "",
+		icon_close_tab_modified = "●",
+		icon_pinned = "車",
 
 		-- If true, new buffers will be inserted at the end of the list.
 		-- Default is to insert after current buffer.
@@ -154,7 +158,7 @@ function config.barbar()
 		-- New buffer letters are assigned in this order. This order is
 		-- optimal for the qwerty keyboard layout but might need adjustement
 		-- for other layouts.
-		letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+		letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
 
 		-- Sets the name of unnamed buffers. By default format is "[Buffer X]"
 		-- where X is the buffer number. But only a static string is accepted here.
@@ -163,7 +167,7 @@ function config.barbar()
 end
 
 function config.hop()
-	require'hop'.setup()
+	require("hop").setup()
 end
 
 return config
