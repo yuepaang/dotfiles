47e3258b60e86d837469d2386b410c9cf5a9d2f8
Yue Peng <yuepaang@gmail.com>
2022-05-16 22:02:02 +0800

    update lsp

--- neovim/lua/config/lsp/init.lua
+++ neovim/lua/config/lsp/init.lua
@@ -1,65 +1,67 @@
 local M = {}
 
 local servers = {
-  gopls = {},
-  html = {},
-  jsonls = {
-    settings = {
-      json = {
-        schemas = require("schemastore").json.schemas(),
-      },
+    gopls = {},
+    html = {},
+    jsonls = {
+        settings = {
+            json = {
+                schemas = require("schemastore").json.schemas(),
+            },
+        },
     },
-  },
-  pyright = {},
-  rust_analyzer = {
-    settings = {
-      ["rust-analyzer"] = {
-        cargo = { allFeatures = true },
-        checkOnSave = {
-          allFeatures = true,
-          command = "clippy",
-          extraArgs = { "--no-deps" },
+    pyright = {},
+    rust_analyzer = {
+        settings = {
+            ["rust-analyzer"] = {
+                cargo = { allFeatures = true },
+                checkOnSave = {
+                    allFeatures = true,
+                    command = "clippy",
+                    extraArgs = { "--no-deps" },
+                },
+                procMacro = {
+                    ignored = {
+                        ["async-trait"] = { "async_trait" },
+                        ["napi-derive"] = { "napi" },
+                        ["async-recursion"] = { "async_recursion" },
+                    },
+                },
+            },
         },
-        procMacro = {
-          ignored = {
-            ["async-trait"] = { "async_trait" },
-            ["napi-derive"] = { "napi" },
-            ["async-recursion"] = { "async_recursion" },
-          },
-        },
-      },
     },
-  },
-  sumneko_lua = {
-    settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = "LuaJIT",
-          -- Setup your lua path
-          path = vim.split(package.path, ";"),
+    sumneko_lua = {
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = "LuaJIT",
+                    -- Setup your lua path
+                    path = vim.split(package.path, ";"),
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = { "vim", "PLUGINS", "dump" },
+                    disable = { "lowercase-global" },
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = {
+                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                    },
+                    maxPreload = 2000,
+                    preloadFileSize = 50000,
+                },
+                completion = { callSnippet = "Both", enable = true, showWord = "Disable" },
+                telemetry = { enable = false },
+            },
         },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = { "vim", "PLUGINS", "dump" },
-          disable = { "lowercase-global" },
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = {
-            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-          },
-          maxPreload = 2000,
-          preloadFileSize = 50000,
-        },
-        completion = { callSnippet = "Both", enable = true, showWord = "Disable" },
-        telemetry = { enable = false },
-      },
     },
-  },
-  tsserver = {},
-  vimls = {},
+    tsserver = {},
+    vimls = {},
+    tailwindcss = {},
+    solang = {},
 }
 
 -- local lsp_signature = require "lsp_signature"
@@ -71,49 +73,49 @@ local servers = {
 -- }
 
 local function on_attach(client, bufnr)
-  -- Enable completion triggered by <C-X><C-O>
-  -- See `:help omnifunc` and `:help ins-completion` for more information.
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+    -- Enable completion triggered by <C-X><C-O>
+    -- See `:help omnifunc` and `:help ins-completion` for more information.
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Use LSP as the handler for formatexpr.
-  -- See `:help formatexpr` for more information.
-  vim.api.nvim_buf_set_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+    -- Use LSP as the handler for formatexpr.
+    -- See `:help formatexpr` for more information.
+    vim.api.nvim_buf_set_option(0, "formatexpr", "v:lua.vim.lsp.formatexpr()")
 
-  -- Configure key mappings
-  require("config.lsp.keymaps").setup(client, bufnr)
+    -- Configure key mappings
+    require("config.lsp.keymaps").setup(client, bufnr)
 
-  -- Configure highlighting
-  require("config.lsp.highlighter").setup(client)
+    -- Configure highlighting
+    require("config.lsp.highlighter").setup(client)
 
-  -- Configure formatting
-  require("config.lsp.null-ls.formatters").setup(client, bufnr)
+    -- Configure formatting
+    require("config.lsp.null-ls.formatters").setup(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 if PLUGINS.nvim_cmp.enabled then
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities) -- for nvim-cmp
+    capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities) -- for nvim-cmp
 end
 
 local opts = {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    allow_incremental_sync = true,
-    debounce_text_changes = 200,
-  },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+        allow_incremental_sync = true,
+        debounce_text_changes = 200,
+    },
 }
 
 -- Setup LSP handlers
 require("config.lsp.handlers").setup()
 
 function M.setup()
-  -- null-ls
-  require("config.lsp.null-ls").setup(opts)
+    -- null-ls
+    require("config.lsp.null-ls").setup(opts)
 
-  -- Installer
-  require("config.lsp.installer").setup(servers, opts)
+    -- Installer
+    require("config.lsp.installer").setup(servers, opts)
 end
 
 return M
