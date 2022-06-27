385c724ebbd52745c059ac2f4adbab1597237c11
Yue Peng <yuepaang@gmail.com>
2022-06-27 09:45:24 +0800

    update doodleVim

--- nvim/lua/doodleVim/core/options.lua
+++ nvim/lua/doodleVim/core/options.lua
@@ -138,7 +138,7 @@ M.load_options = function()
     vim.g.python_host_skip_check = 1
     vim.g.python_host_prog = "/usr/bin/python"
     vim.g.python3_host_skip_check = 1
-    vim.g.python3_host_prog = "/usr/bin/python3"
+    vim.g.python3_host_prog = "~/.pyenv/versions/nvim-py3/bin/python3"
   end
 
   for name, value in pairs(global_local) do
