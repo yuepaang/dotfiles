54577a98fb9f3d79a5b9aa1bf329034107962242
Yue Peng <yuepaang@gmail.com>
2022-07-10 15:57:50 +0800

    update nvim

--- nvim/lua/doodleVim/modules/debug/plugins.lua
+++ nvim/lua/doodleVim/modules/debug/plugins.lua
@@ -1,11 +1,12 @@
 local debug = {}
-local conf = require("doodleVim.modules.debug.config")
+local conf = require "doodleVim.modules.debug.config"
 
 debug["mfussenegger/nvim-dap"] = {
   opt = true,
-  setup = function()
-    require("doodleVim.utils.defer").defer_load("nvim-dap", 500)
-  end,
+  after = "mason.nvim",
+  -- setup = function()
+  --   require("doodleVim.utils.defer").defer_load("nvim-dap", 500)
+  -- end,
   config = conf.dap,
 }
 
