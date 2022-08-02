497d542bf954f8050263259e269e06194dbedd07
Yue Peng <yuepaang@gmail.com>
2022-08-03 01:02:30 +0800

    use normal gd

--- neovim/lua/config/lsp/keymaps.lua
+++ neovim/lua/config/lsp/keymaps.lua
@@ -44,8 +44,8 @@ local function keymappings(client, bufnr)
 
   local keymap_g = {
     name = "Goto",
-    -- d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
-    d = { "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "Definition" },
+    d = { "<Cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
+    -- d = { "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "Definition" },
     D = { "<Cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
     h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
     I = { "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation" },
