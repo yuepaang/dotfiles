02769ff9443f0b961726838f946d82320db69cce
Yue Peng <yuepeng@sfmail.sf-express.com>
2022-08-05 22:04:54 +0800

    update doodle

--- nvim/lua/doodleVim/extend/misc.lua
+++ nvim/lua/doodleVim/extend/misc.lua
@@ -1,94 +1,94 @@
-local utils = require "doodleVim.utils.utils"
+local utils = require("doodleVim.utils.utils")
 
 local misc = {}
 misc.which_key_loaded = false
 
 misc.safe_exit = function()
-    require("doodleVim.utils.defer").immediate_load "vim-floaterm"
+  require("doodleVim.utils.defer").immediate_load({ "vim-floaterm", "nvim-tree.lua" })
 
-    -- close floaterm
-    local floatermBufnr = vim.call "floaterm#buflist#gather"
-    if #floatermBufnr ~= 0 then
-        vim.cmd [[FloatermKill]]
-    end
+  -- close floaterm
+  local floatermBufnr = vim.call("floaterm#buflist#gather")
+  if #floatermBufnr ~= 0 then
+    vim.cmd([[FloatermKill]])
+  end
 
-    -- close nvim-tree
-    local view = ensure_require "nvim-tree.view"
-    if view.is_visible() then
-        require("doodleVim.extend.tree").toggle()
-    end
+  -- close nvim-tree
+  local view = require("nvim-tree.view")
+  if view.is_visible() then
+    require("doodleVim.extend.tree").toggle()
+  end
 
-    -- quit
-    vim.cmd [[confirm qa]]
+  -- quit
+  vim.cmd([[confirm qa]])
 end
 
 misc.safe_save = function()
-    require("doodleVim.utils.defer").immediate_load "auto-session"
-    vim.cmd [[write]]
-    require("auto-session").SaveSession()
-    vim.notify "Current Session Saved"
+  require("doodleVim.utils.defer").immediate_load("auto-session")
+  vim.cmd([[write]])
+  require("auto-session").SaveSession()
+  vim.notify("Current Session Saved")
 end
 
 misc.wrapped_notify = function(m, l, o)
-    if m:match("NULL_LS_CODE_ACTION", 1) then
-        return
-    end
-    local wrapped_msg = require("doodleVim.utils.utils").wrap(m, 52)
-    require "notify" (wrapped_msg, l, o)
+  if m:match("NULL_LS_CODE_ACTION", 1) then
+    return
+  end
+  local wrapped_msg = require("doodleVim.utils.utils").wrap(m, 52)
+  require("notify")(wrapped_msg, l, o)
 end
 
 misc.toggle_nu = function()
-    vim.cmd [[:set nornu]]
-    if vim.wo.nu then
-        vim.cmd [[:set nonu]]
-    else
-        vim.cmd [[:set nu]]
-    end
+  vim.cmd([[:set nornu]])
+  if vim.wo.nu then
+    vim.cmd([[:set nonu]])
+  else
+    vim.cmd([[:set nu]])
+  end
 end
 
 misc.toggle_rnu = function()
-    vim.cmd [[:set nonu]]
-    if vim.wo.rnu then
-        vim.cmd [[:set nornu]]
-    else
-        vim.cmd [[:set rnu]]
-    end
+  vim.cmd([[:set nonu]])
+  if vim.wo.rnu then
+    vim.cmd([[:set nornu]])
+  else
+    vim.cmd([[:set rnu]])
+  end
 end
 
 misc.reload = function()
-    utils.remove_cached_package "^doodleVim"
-    require "doodleVim.core"
-    vim.notify "Configuration Reloaded"
+  utils.remove_cached_package("^doodleVim")
+  require("doodleVim.core")
+  vim.notify("Configuration Reloaded")
 end
 
 misc.enhanced_buffer_close = function()
-    require("doodleVim.utils.defer").immediate_load "barbar.nvim"
+  require("doodleVim.utils.defer").immediate_load("barbar.nvim")
 
-    local win_num = vim.api.nvim_win_get_number(0)
-    local filetype = vim.bo.filetype
-    if filetype == "TelescopePrompt" then
-        return
-    end
+  local win_num = vim.api.nvim_win_get_number(0)
+  local filetype = vim.bo.filetype
+  if filetype == "TelescopePrompt" then
+    return
+  end
 
-    if win_num > 2 then
-        vim.cmd [[bdelete!]]
-    else
-        vim.cmd [[BufferClose]]
-    end
+  if win_num > 2 then
+    vim.cmd([[bdelete!]])
+  else
+    vim.cmd([[BufferClose]])
+  end
 end
 
 misc.toggle_whichkey = function()
-    local mode = vim.api.nvim_get_mode()
-    if misc.which_key_loaded then
-        require("which-key.view").on_close()
-        misc.which_key_loaded = false
-    else
-        if mode.mode == "V" or mode.mode == "\22" then
-            mode.mode = "v"
-        end
-        require("which-key").show("", mode)
-        misc.which_key_loaded = true
+  local mode = vim.api.nvim_get_mode()
+  if misc.which_key_loaded then
+    require("which-key.view").on_close()
+    misc.which_key_loaded = false
+  else
+    if mode.mode == "V" or mode.mode == "\22" then
+      mode.mode = "v"
     end
+    require("which-key").show("", mode)
+    misc.which_key_loaded = true
+  end
 end
 
 return misc
--- nvim/lua/doodleVim/modules/ui/alpha.lua
+++ nvim/lua/doodleVim/modules/ui/alpha.lua
@@ -2,20 +2,20 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
 local logo = {
-    "                  ███████   ██    ██                 ",
-    "                  ██   ██    ██  ██                  ",
-    "                  ███████     ████                   ",
-    "                  ██           ██                    ",
-    "                  ██           ██                    ",
-    "                                                     ",
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                     ",
+  "                  ███████   ██    ██                 ",
+  "                  ██   ██    ██  ██                  ",
+  "                  ███████     ████                   ",
+  "                  ██           ██                    ",
+  "                  ██           ██                    ",
+  "                                                     ",
+  "                                                     ",
+  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+  "                                                     ",
 }
 
 -- local py_logo = {
@@ -41,68 +41,65 @@ local logo = {
 -- }
 
 local function footer()
-    local datetime = os.date(" %Y-%m-%d") .. "  -  "
-    local author = " " .. "yuepaang" .. "  -  "
-    local total_plugins = " " .. #vim.tbl_keys(packer_plugins) .. " plugins" .. "  -  "
-    local version = vim.version()
-    local nvim_version_info = " v" .. version.major .. "." .. version.minor .. "." .. version.patch .. "\n"
-    -- Quote
-    local fortune = require("alpha.fortune")
-    local quote = table.concat(fortune(), "\n")
+  local datetime = os.date(" %Y-%m-%d") .. "  -  "
+  local author = " " .. "yuepaang" .. "  -  "
+  local total_plugins = " " .. #vim.tbl_keys(packer_plugins) .. " plugins" .. "  -  "
+  local version = vim.version()
+  local nvim_version_info = " v" .. version.major .. "." .. version.minor .. "." .. version.patch .. "\n"
+  -- Quote
+  local fortune = require("alpha.fortune")
+  local quote = table.concat(fortune(), "\n")
 
-    return datetime .. author .. total_plugins .. nvim_version_info .. quote
+  return datetime .. author .. total_plugins .. nvim_version_info .. quote
 end
 
 dashboard.section.header.val = logo
 
 dashboard.section.buttons.val = {
-    dashboard.button("o", "  Open CWD",        "<cmd>ene|OpenTree<CR>"),
-    dashboard.button("p", "  Recent Projects", "<cmd>Telescope projects<CR>"),
-    dashboard.button("r", "  Recent File",     "<cmd>Telescope oldfiles<CR>"),
-    dashboard.button("e", "  New file",        "<cmd>ene<CR>"),
-    dashboard.button("f", "  Find File",       "<cmd>Telescope find_files<CR>"),
-    dashboard.button("b", "  File Browser",    "<cmd>Telescope file_browser<CR>"),
-    dashboard.button("s", "  Configuration",   "<cmd>e $MYVIMRC|OpenTree<CR>"),
-    dashboard.button("u", "  Update Plugins",  "<cmd>PackerUpdate<CR>"),
-    dashboard.button("q", "  Quit",            "<cmd>qa<cr>"),
+  dashboard.button("o", "  Open CWD", "<cmd>ene|OpenTree<CR>"),
+  dashboard.button("p", "  Recent Projects", "<cmd>Telescope projects<CR>"),
+  dashboard.button("r", "  Recent File", "<cmd>Telescope oldfiles<CR>"),
+  dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+  dashboard.button("f", "  Find File", "<cmd>Telescope find_files<CR>"),
+  dashboard.button("b", "  File Browser", "<cmd>Telescope file_browser<CR>"),
+  dashboard.button("s", "  Configuration", "<cmd>e $MYVIMRC|OpenTree<CR>"),
+  dashboard.button("u", "  Update Plugins", "<cmd>PackerUpdate<CR>"),
+  dashboard.button("q", "  Quit", "<cmd>qa<cr>"),
 }
 
 dashboard.section.footer.val = footer()
 dashboard.section.footer.opts.hl = "Constant"
 
-
 dashboard.opts = {
-    layout = {
-        { type = "padding", val = 4 },
-        dashboard.section.header,
-        { type = "padding", val = 4 },
-        dashboard.section.buttons,
-        { type = "padding", val = 2 },
-        dashboard.section.footer,
-    },
-    opts = {
-        margin = 5,
-    },
+  layout = {
+    { type = "padding", val = 4 },
+    dashboard.section.header,
+    { type = "padding", val = 4 },
+    dashboard.section.buttons,
+    { type = "padding", val = 2 },
+    dashboard.section.footer,
+  },
+  opts = {
+    margin = 5,
+  },
 }
 
 alpha.setup(dashboard.opts)
 
 vim.api.nvim_create_augroup("alpha_tabline", { clear = true })
-
 vim.api.nvim_create_autocmd("FileType", {
-    group = "alpha_tabline",
-    pattern = "alpha",
-    command = "set showtabline=0 laststatus=0 noruler",
+  group = "alpha_tabline",
+  pattern = "alpha",
+  command = "set showtabline=0 laststatus=0 noruler",
 })
-
 vim.api.nvim_create_autocmd("FileType", {
-    group = "alpha_tabline",
-    pattern = "alpha",
-    callback = function()
-        vim.api.nvim_create_autocmd("BufUnload", {
-            group = "alpha_tabline",
-            buffer = 0,
-            command = "set showtabline=2 ruler laststatus=3",
-        })
-    end,
+  group = "alpha_tabline",
+  pattern = "alpha",
+  callback = function()
+    vim.api.nvim_create_autocmd("BufUnload", {
+      group = "alpha_tabline",
+      buffer = 0,
+      command = "set showtabline=2 ruler laststatus=3",
+    })
+  end,
 })
