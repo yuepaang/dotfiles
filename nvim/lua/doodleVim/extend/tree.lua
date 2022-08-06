d2d5e6b420573eeb8d856495023ddf56b4ae2f34
Yue Peng <yuepaang@gmail.com>
2022-08-06 13:46:22 +0800

    update doodle

--- nvim/lua/doodleVim/extend/tree.lua
+++ nvim/lua/doodleVim/extend/tree.lua
@@ -1,6 +1,10 @@
 local tree = {}
 
 tree.toggle = function()
+  if vim.bo.filetype == "alpha" then
+    return
+  end
+
   require("doodleVim.utils.defer").immediate_load({ "barbar.nvim", "nvim-tree.lua" })
   local api = require("nvim-tree.api")
   api.tree.toggle()
