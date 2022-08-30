b43846fd8f79f9b9b7caea76d6a14f6cd258c734
Yue Peng <yuepeng@sfmail.sf-express.com>
2022-08-30 10:13:22 +0800

    update neovim

--- neovim/lua/utils/treesitter.lua
+++ neovim/lua/utils/treesitter.lua
@@ -4,9 +4,11 @@ local python_function_query_string = [[
   (function_definition) @function
 ]]
 
+-- local ts_locals = require "nvim-treesitter.locals"
+-- local ts_utils = require "nvim-treesitter.ts_utils"
 local q = require "vim.treesitter.query"
 
-local bufnr = 99
+local bufnr_test = 99
 
 function M.get_functions(bufnr, lang, query_string)
   local parser = vim.treesitter.get_parser(bufnr, lang)
@@ -17,12 +19,14 @@ function M.get_functions(bufnr, lang, query_string)
   -- for id, node in query:iter_captures(root, bufnr, 0, -1) do
   --   dump(q.get_node_text(node, bufnr))
   -- end
+  dump(q)
 
   for _, captures, metadata in query:iter_matches(root, bufnr) do
     dump(q.get_node_text(captures[1], bufnr))
+    dump(metadata)
   end
 end
 
-M.get_functions(bufnr, "python", python_function_query_string)
+M.get_functions(bufnr_test, "python", python_function_query_string)
 
 return M
