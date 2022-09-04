3808df3e75877efc1b519cb748d230903e615407
Yue Peng <yuepaang@gmail.com>
2022-09-04 14:32:55 +0800

    update nvb

--- nvb/lua/config/treesitter.lua
+++ nvb/lua/config/treesitter.lua
@@ -60,50 +60,55 @@ function M.setup()
           ["af"] = "@function.outer",
           ["if"] = "@function.inner",
           ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        },
+        selection_modes = {
+          ["@parameter.outer"] = "v", -- charwise
+          ["@function.outer"] = "V", -- linewise
+          ["@class.outer"] = "<c-v>", -- blockwise
         },
       },
-
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>cx"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>cX"] = "@parameter.inner",
-        },
-      },
-
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-      },
-
-      -- lsp_interop = {
-      --   enable = true,
-      --   border = "none",
-      --   peek_definition_code = {
-      --     ["<leader>cf"] = "@function.outer",
-      --     ["<leader>cF"] = "@class.outer",
-      --   },
-      -- },
     },
+
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>cx"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>cX"] = "@parameter.inner",
+      },
+    },
+
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+
+    -- lsp_interop = {
+    --   enable = true,
+    --   border = "none",
+    --   peek_definition_code = {
+    --     ["<leader>cf"] = "@function.outer",
+    --     ["<leader>cF"] = "@class.outer",
+    --   },
+    -- },
 
     -- endwise
     endwise = {
