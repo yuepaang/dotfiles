7d0007956052d774a874be226dc0ed08976828c4
Yue Peng <yuepeng@sfmail.sf-express.com>
2022-10-10 11:19:24 +0800

    update nvim

--- nvim/lua/doodleVim/keymap/map.lua
+++ nvim/lua/doodleVim/keymap/map.lua
@@ -63,7 +63,7 @@ map.Lsp = {
         map_cu("lua vim.lsp.buf.formatting()"):with_noremap():with_silent():with_label("Selected Code Formatting")
       ),
       a = bind.convert_wk_format(
-        map_cu("lua vim.lsp.buf.range_code_action()"):with_noremap():with_silent():with_label("Range Code Action")
+        map_cu("lua vim.lsp.buf.code_action()"):with_noremap():with_silent():with_label("Range Code Action")
       ),
       n = bind.convert_wk_format(
         map_cr("lua require('rename').rename()"):with_noremap():with_silent():with_label("Rename")
