3b1d0600866569a7e3689098e9d343f9ecebecc0
Yue Peng <yuepeng@sfmail.sf-express.com>
2022-11-28 11:19:45 +0800

    update lvim

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -176,3 +176,8 @@ vim.api.nvim_create_autocmd(
   "BufReadPost",
   { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
 )
+-- create directories when needed, when saving a file
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  command = [[call mkdir(expand('<afile>:p:h'), 'p')]],
+})
