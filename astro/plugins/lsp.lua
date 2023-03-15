59177ac75f64bd9c9781e373caa7edc35dee7fa3
Yue Peng <yuepeng@sfmail.sf-express.com>
2023-03-15 18:09:57 +0800

    update nvim

---
+++ astro/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+	-- {
+	-- 	"williamboman/mason-lspconfig.nvim",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"lua-language-server",
+	-- 			"rust_analyzer",
+	-- 		},
+	-- 	},
+	-- },
+	-- {"neovim/nvim-lspconfig",}
+	{ "lvimuser/lsp-inlayhints.nvim", config = true },
+	{
+		"Maan2003/lsp_lines.nvim",
+		event = "User AstroFile",
+		config = function()
+			require("lsp_lines").setup()
+		end,
+	},
+	{
+		"simrat39/rust-tools.nvim",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		ft = { "rs" },
+		opts = function()
+			return {
+				server = astronvim.lsp.server_settings("rust_analyzer"),
+			}
+		end,
+	},
+}
