f705fe23cd4f1e1bf6d81a05d2120b878c01d708
Yue Peng <yuepaang@gmail.com>
2023-03-29 23:15:26 +0800

    update astro

--- astro/plugins/editor.lua
+++ astro/plugins/editor.lua
@@ -68,7 +68,7 @@ return {
 		"ahmedkhalf/project.nvim",
 		event = "VeryLazy",
 		opts = {
-			ignore_lsp = { "lua_ls" },
+			ignore_lsp = { "lua_ls", "julials" },
 		},
 		config = function(_, opts)
 			require("project_nvim").setup(opts)
