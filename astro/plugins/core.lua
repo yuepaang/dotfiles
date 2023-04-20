e245713f085c038b893e03c8bdd6ab9e44cec5d0
Yue Peng <yuepeng@sfmail.sf-express.com>
2023-04-20 15:14:15 +0800

    update astro

--- astro/plugins/core.lua
+++ astro/plugins/core.lua
@@ -1,37 +1,42 @@
 return { -- { "goolord/alpha-nvim", enabled = false },
--- { "max397574/better-escape.nvim", enabled = false },
-{
-    "akinsho/toggleterm.nvim",
-    opts = {
-        terminal_mappings = false
-    }
-}, {
-    "rcarriga/nvim-notify",
-    opts = {
-        timeout = 0
-    }
-}, {
-    "mrjones2014/smart-splits.nvim",
-    build = "./kitty/install-kittens.bash",
-    opts = {
-        at_edge = "split"
-    }
-}, {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-        signcolumn = false,
-        numhl = true,
-        current_line_blame_opts = {
-            ignore_whitespace = true
-        }
-    }
-}, {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-        filesystem = {
-            filtered_items = {
-                always_show = {".github", ".gitignore"}
-            }
-        }
-    }
-}}
+	-- { "max397574/better-escape.nvim", enabled = false },
+	{
+		"akinsho/toggleterm.nvim",
+		opts = {
+			terminal_mappings = false,
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 0,
+		},
+	},
+	-- {
+	-- 	"mrjones2014/smart-splits.nvim",
+	-- 	build = "./kitty/install-kittens.bash",
+	-- 	opts = {
+	-- 		at_edge = "split",
+	-- 	},
+	-- },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signcolumn = false,
+			numhl = true,
+			current_line_blame_opts = {
+				ignore_whitespace = true,
+			},
+		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					always_show = { ".github", ".gitignore" },
+				},
+			},
+		},
+	},
+}
