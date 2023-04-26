fd4dea4caf5b6431e65b78b759aaeccd65f547bc
Yue Peng <yuepeng@sfmail.sf-express.com>
2023-04-26 10:22:12 +0800

    update lvim astro

--- astro/plugins/treesitter.lua
+++ astro/plugins/treesitter.lua
@@ -8,17 +8,6 @@ return {
 				vim.g.matchup_matchparen_deferred = 1
 			end,
 		},
-		{
-			"HiPhish/nvim-ts-rainbow2",
-			config = function()
-				vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
-					callback = function()
-						vim.cmd.TSDisable("rainbow")
-						vim.cmd.TSEnable("rainbow")
-					end,
-				})
-			end,
-		},
 	},
 	opts = {
 		auto_install = vim.fn.executable("tree-sitter") == 1,
