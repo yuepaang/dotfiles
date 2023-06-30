cf840fbde561892cd05e60d6208459aeca726fef
Yue Peng <yuepeng@sfmail.sf-express.com>
2023-06-30 10:52:12 +0800

    pooling ids

--- lvim/lua/user/plugins.lua
+++ lvim/lua/user/plugins.lua
@@ -38,7 +38,10 @@ lvim.plugins = {
   "nvim-treesitter/nvim-treesitter-textobjects",
   "mfussenegger/nvim-jdtls",
   "opalmay/vim-smoothie",
-  "j-hui/fidget.nvim",
+  {
+    "j-hui/fidget.nvim",
+    branch = "legacy",
+  },
   "windwp/nvim-ts-autotag",
   "kylechui/nvim-surround",
   "christianchiarulli/harpoon",
@@ -125,6 +128,7 @@ lvim.plugins = {
     "hrsh7th/cmp-emoji",
     event = "BufRead",
   },
+  "ThePrimeagen/vim-be-good",
   -- "folke/noice.nvim",
   -- "rcarriga/nvim-notify",
 
