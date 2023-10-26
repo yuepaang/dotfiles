c4cde654a40782c0445e8988422a23c152943515
Yue Peng <yuepeng@sf-express.com>
2023-10-26 09:58:13 +0800

    update

--- nvim/lua/doodleVim/modules/tools/config.lua
+++ nvim/lua/doodleVim/modules/tools/config.lua
@@ -213,7 +213,6 @@ function config.nvim_tree()
       adaptive_size = false,
       centralize_selection = true,
       width = 30,
-      hide_root_folder = false,
       side = "left",
       preserve_window_proportions = false,
       number = false,
